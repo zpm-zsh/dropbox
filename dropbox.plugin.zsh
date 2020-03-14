16f047dae32830e2e1394d8352c241365ebd415a
Grigorii Horos <horosgrisa@gmail.com>
2020-03-14 20:18:49 +0200

    Optimize

---
+++ dropbox.plugin.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+# Standarized $0 handling, following:
+# https://github.com/zdharma/Zsh-100-Commits-Club/blob/master/Zsh-Plugin-Standard.adoc
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+if [[ $PMSPEC != *b* ]] {
+  PATH=$PATH:"${0:h}/bin"
+}
+
+if [[ $PMSPEC != *f* ]] {
+  fpath+=( "${0:h}/functions" )
+}
